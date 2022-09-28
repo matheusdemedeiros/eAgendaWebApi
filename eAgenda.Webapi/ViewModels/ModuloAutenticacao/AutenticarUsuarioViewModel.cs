f7dafc05db545568278b491df91f1acb86a69865
Matheus <matheusdemedeiros200599@gmail.com>
2022-09-27 21:03:48 -0300

    member feat (autenticação): criação das ViewModels de autenticação e seu mapeamento com autoMapper.

---
+++ eAgenda.Webapi/ViewModels/ModuloAutenticacao/AutenticarUsuarioViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace eAgenda.Webapi.ViewModels.ModuloAutenticacao
+{
+    public class AutenticarUsuarioViewModel
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
---
+++ eAgenda.Webapi/ViewModels/ModuloAutenticacao/TokenViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace eAgenda.Webapi.ViewModels.ModuloAutenticacao
+{
+    public class TokenViewModel
+    {
+        public string Chave { get; set; }
+        public UsuarioTokenViewModel UsuarioToken { get; set; }
+    }
+}
