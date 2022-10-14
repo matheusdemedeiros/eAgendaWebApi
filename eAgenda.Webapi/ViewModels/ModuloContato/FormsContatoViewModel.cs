a67c6584166ef05b2ff1112f69d9ee3b692564da
Matheus <matheusdemedeiros200599@gmail.com>
2022-10-14 11:11:10 -0300

    add  id no formsViewModel contato  (edição).

--- eAgenda.Webapi/ViewModels/ModuloContato/FormsContatoViewModel.cs
+++ eAgenda.Webapi/ViewModels/ModuloContato/FormsContatoViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System;
 
 namespace eAgenda.Webapi.ViewModels.ModuloContato
 {
     public class FormsContatoViewModel
     {
+        
+        public Guid Id { get; set; }
         public string Nome { get; set; }
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public string Email { get; set; }
